4e72ab522f19fad86f19c24d84cef38cf1189e21
glalagladi <68953521+glalagladi@users.noreply.github.com>
2022-06-13 20:00:42 +0200

    optimized

--- src/main/java/net/mcreator/derimcraft/block/FakeDiamondBlockBlock.java
+++ src/main/java/net/mcreator/derimcraft/block/FakeDiamondBlockBlock.java
@@ -1,15 +1,22 @@
 
 package net.mcreator.derimcraft.block;
 
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.FallingBlock;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.core.BlockPos;
+
+import java.util.List;
+import java.util.Collections;
 
 public class FakeDiamondBlockBlock extends FallingBlock {
-
 	public FakeDiamondBlockBlock() {
 		super(BlockBehaviour.Properties.of(Material.DIRT).sound(SoundType.GRAVEL).strength(1f, 10f));
-
 	}
 
 	@Override
@@ -19,11 +26,9 @@ public class FakeDiamondBlockBlock extends FallingBlock {
 
 	@Override
 	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 		if (!dropsOriginal.isEmpty())
 			return dropsOriginal;
 		return Collections.singletonList(new ItemStack(this, 1));
 	}
-
 }
